4878f9cfb15b20db44a8a0a307afe76c74e4dffa
louisnavarre <navarre.louis@student.uclouvain.be>
2019-02-23 10:50:02 +0100

    first layout

--- app/src/main/java/epl/students/programmers/gridflare/MainActivity.java
+++ app/src/main/java/epl/students/programmers/gridflare/MainActivity.java
@@ -2,6 +2,10 @@ package epl.students.programmers.gridflare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Launch a test");
+    }
+
+    public void launch_a_test(View v){
+        Toast.makeText(getBaseContext(), "Test in progress. Stay where you are.",
+                Toast.LENGTH_LONG).show();
+        TextView value = findViewById(R.id.launch_value);
+        Random rand = new Random();
+        int va = rand.nextInt();
+        value.setText(""+va);
     }
 }
